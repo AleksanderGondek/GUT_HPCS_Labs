78d67e5c8b2bffaded3468f1e09cb16dd4e81df8
Alex <gondekaleksander@hotmail.com>
2015-03-22 14:25:32 +0100

    Added files for LabOne - vanilla and one modifed to use basic Leibneitz formula

---
+++ LabOne/count_pi.c
@@ -0,0 +1,60 @@
+#include <mpi.h>
+#include <stdio.h>
+
+#define _PRECISION_ 1000000000;
+
+int main(int argc, char **argv)
+{
+	double precision = _PRECISION_;
+	int  my_rank, proc_count;
+	int mine, sign;
+	double pi, pi_final;
+	int i;
+
+	//Initialize MPI
+	MPI_Init(&argc, &argv);
+
+	//Get process id
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	//Get number of all processes
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+
+	// Distribute required precision
+	if(precision < proc_count)
+	{
+		printf("Precision smaller than the number of processes - try again");
+		MPI_Finalize();
+		return -1;
+	}
+
+	//Each process do computing for itself
+	pi = 0;
+	mine = my_rank *2 + 1;
+	sign = (( (mine-1)/2 ) % 2) ? -1 : 1;
+
+	for(;mine<precision;)
+	{
+		// printf("\n Process rank %d  \n Process sign %d \n Process mine: %d \n", my_rank, sign, mine);
+		// fflush(stdout);
+
+		pi += sign / (double)mine;
+		mine += 2 * proc_count;
+		sign = ( ( (mine-1)/2 ) %2 ) ? -1 : 1;
+	}
+
+	// Mergin all result to that in proc id = 0
+
+	MPI_Reduce(&pi, &pi_final,1,MPI_DOUBLE, MPI_SUM,0, MPI_COMM_WORLD);
+
+	// In human speech - if proc id = 0
+	if(!my_rank)
+	{
+		pi_final *= 4;
+		printf("pi = %f", pi_final);
+	}
+
+	//Shutdown MPI
+	MPI_Finalize();
+	return 0;
+}
---
+++ LabOne/rational_count_pi.c
@@ -0,0 +1,61 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <math.h>
+
+#define _PRECISION_ 1000000000;
+
+int main(int argc, char **argv)
+{
+	double precision = _PRECISION_;
+	int  my_rank, proc_count;
+	double pi_final;
+
+	//My variables
+	int myN;
+	double myLocalSum, nThElement;
+
+	//Initialize MPI
+	MPI_Init(&argc, &argv);
+
+	//Get process id
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+	//Get number of all processes
+	MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
+
+	// Distribute required precision
+	if(precision < proc_count)
+	{
+		printf("Precision smaller than the number of processes - try again");
+		MPI_Finalize();
+		return -1;
+	}
+
+	//Leibnitz formula instead of randomness
+	myN = my_rank;
+	myLocalSum = 0;
+
+	while(myN < precision)
+	{
+		nThElement = ( ( pow(-1.0, myN) ) / ( (2 * myN) + 1 ) );
+		myLocalSum = myLocalSum + nThElement;
+		myN = myN + proc_count;
+	}
+
+	// printf("\n Process rank %d  \n Process sign %d \n Process mine: %d \n", my_rank, sign, mine);
+	// fflush(stdout);
+
+	// Mergin all result to that in proc id = 0
+	MPI_Reduce(&myLocalSum, &pi_final,1,MPI_DOUBLE, MPI_SUM,0, MPI_COMM_WORLD);
+
+	// In human speech - if proc id = 0
+	if(!my_rank)
+	{
+		pi_final *= 4;
+		printf("pi = %f", pi_final);
+	}
+
+	//Shutdown MPI
+	MPI_Finalize();
+	return 0;
+}
